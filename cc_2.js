74b98c02ec3c8e5346de4c17ef7a4964944116e4
Your Name <your.email@example.com>
2025-01-24 17:15:26 -0500

    Task 2 - Student Scores

--- cc_2.js
+++ cc_2.js
@@ -2,3 +2,14 @@ let products = ["headphones","earbuds", "speaker", "microphone","mouse"];
 products.unshift("keyboard");
 products.pop();
 console.log("Updated Products", products);
+
+let scores = [97,65,78,100,87]
+scores[1] = 70
+
+let total = scores[0] + scores[1] + scores[2] + scores[3] + scores[4]
+let Average = total / scores.length
+
+console.log("Updated Scores", scores)
+console.log("Average Scores", Average)
+
+
