50d0b22a7b2405c0dc945d728ce66fe6d022698c
Your Name <your.email@example.com>
2025-01-26 22:32:43 -0500

    Task 5 - Order Processing System

--- cc_2.js
+++ cc_2.js
@@ -6,7 +6,6 @@ console.log("Updated Products", products);
 let scores = [97,65,78,100,87];
 scores[1] = 70;
 
-
 let total = scores.reduce((total,amount) => total + amount, 0)
 let Average = total / scores.length
 console.log("Total Scores", total);
@@ -18,11 +17,17 @@ let employee = { name: "Peter Parker", age: 20, department: "Avenger", isActive:
 employee.department = "Intern"
 console.log(employee);
 
-let customers = [{ name: "Green Goblin", email: "goblin@gmail.com", purchaseAmount: "700"} , 
-    { name: "Kim Kardashian", email: "kimk@gmail.com", purchaseAmount: "1000"}, {name: "Jon Snow", email: "snow@gmail.com", purchaseAmount: "10" }]
+let customers = [{ name: "Green Goblin", email: "goblin@gmail.com", purchaseAmount: 700} , 
+    { name: "Kim Kardashian", email: "kimk@gmail.com", purchaseAmount: 1000}, {name: "Jon Snow", email: "snow@gmail.com", purchaseAmount: 10 }];
 
-customers.push({ name: "Michael Jackson", email: "mj1@gmail.com", purchaseAmount: "300"});
+customers.push({ name: "Michael Jackson", email: "mj1@gmail.com", purchaseAmount: 300});
     console.log(customers);
 
+const order = [{ orderID: 20, customerName: "John P", amount: 900 }];
+function calculateTax(amount) {return amount * 0.1}; 
+
+console.log("Order ID number:", order[0].orderID);
+console.log("Customer name:", order[0].customerName);
+console.log("Tax:", calculateTax(order[0].amount));
 
 
